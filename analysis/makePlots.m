1d660262f785d7ef21e576e847fdc77d420954d9
Shane Heiney <sheiney@gmail.com>
2019-01-05 16:25:47 -0600

    Fixed rare plot issue

--- analysis/makePlots.m
+++ analysis/makePlots.m
@@ -20,11 +20,11 @@ hax=axes;
 
 idx = find(trials.c_usnum==us & trials.c_csnum==cs & ismember(trials.session_of_day, session));
 
-set(hax,'ColorOrder',jet(length(idx)),'NextPlot','ReplaceChildren');
-plot(trials.tm(1,:),trials.eyelidpos(idx, :))
+set(hax,'ColorOrder', jet(length(idx)), 'NextPlot', 'ReplaceChildren');
+plot(trials.tm(1,:), trials.eyelidpos(idx, :)')     % transpose matrix before plotting to deal with rare cases where m == n and Matlab chooses to plot columns instead of rows
 hold on 
-plot(trials.tm(1,:),mean(trials.eyelidpos(idx, :)),'k','LineWidth',2)
-axis([trials.tm(1,1) trials.tm(1,end) -0.1 1.1])
+plot(trials.tm(1, :), mean(trials.eyelidpos(idx, :)), 'k', 'LineWidth', 2)
+axis([trials.tm(1, 1) trials.tm(1, end) -0.1 1.1])
 title('CS-US')
 xlabel('Time from CS (s)')
 ylabel('Eyelid pos (FEC)')
@@ -38,15 +38,15 @@ cramp = mean(trials.eyelidpos(:, win), 2) - mean(trials.eyelidpos(:, pre), 2);
 hf2=figure;
 
 idx = find(trials.c_usnum==us & trials.c_csnum==cs & ismember(trials.session_of_day, session));
-plot(trials.trialnum(idx),cramp(idx),'.')
+plot(trials.trialnum(idx), cramp(idx), '.')
 hold on
-plot([1 length(trials.trialnum)],[0.1 0.1],':k')
+plot([1 length(trials.trialnum)], [0.1 0.1], ':k')
 axis([1 length(trials.trialnum) -0.1 1.1])
 title('CS-US')
 xlabel('Trials')
 ylabel('CR size')
 
 if nargout > 0
-    varargout{1}=hf1;
-    varargout{2}=hf2;
+    varargout{1} = hf1;
+    varargout{2} = hf2;
 end
